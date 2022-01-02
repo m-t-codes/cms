2c0c0865c3485c78c1f02a8f09bb86a493936d84
m-t-codes <mt.codes@mailfence.com>
2022-01-02 14:01:09 +0100

    no git ignore

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset ('css/app.css') }}">
+</head>
+<body class="w-max">
+    <h1 class="bg-blue-700 ml-3 w-max">Hallo</h1>
+</body>
+</html>
